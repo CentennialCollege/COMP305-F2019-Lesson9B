50c9f78b5663c64f5e9adba95729bc5b87e01660
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2019-09-14 17:27:29 -0400

    added Island

---
+++ Assets/_Scripts/IslandController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IslandController : MonoBehaviour
+{
+    public float verticalSpeed = 0.05f;
+    public float resetPosition = 2.71f;
+    public float resetPoint = -2.71f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    /// <summary>
+    /// This method moves the ocean down the screen by verticalSpeed
+    /// </summary>
+    void Move()
+    {
+        Vector2 newPosition = new Vector2(0.0f, verticalSpeed);
+        Vector2 currentPosition = transform.position;
+
+        currentPosition -= newPosition;
+        transform.position = currentPosition;
+    }
+
+    /// <summary>
+    /// This method resets the ocean to the resetPosition
+    /// </summary>
+    void Reset()
+    {
+        float randomXPosition = Random.RandomRange(-2.88f, 2.88f);
+        transform.position = new Vector2(randomXPosition, resetPosition);
+    }
+
+    /// <summary>
+    /// This method checks if the ocean reaches the lower boundary
+    /// and then it Resets it
+    /// </summary>
+    void CheckBounds()
+    {
+        if (transform.position.y <= resetPoint)
+        {
+            Reset();
+        }
+    }
+}
